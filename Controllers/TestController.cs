9ba752698721960788af3c0110fb0e884394a6e8
KenDnG <daniswara.kenrick@gmail.com>
2023-01-01 13:45:23 +0700

    c

---
+++ Controllers/TestController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+        public IActionResult Calculate([Bind] TestClass item)
+        {
+            ModelState.Clear();
+            double discountamount = 0;
+            item.qtykecil = item.UOM * item.quantity;
+
+            var splitdisc = item.Discount.Split('+');
+            foreach (var disc in splitdisc)
+            {
+                if (disc.Contains('%'))
+                {
+                    var discnum = Convert.ToInt32(disc.Remove(disc.Length-1));
+                    discountamount += (item.quantity * item.UnitPrice) * discnum / 100;
+                }
+                else
+                {
+                    discountamount += Convert.ToInt32(disc);
+                }
+            }
+            item.DiscAmt = discountamount;
+            item.Total = (item.quantity * item.UnitPrice) - discountamount;
+
+            return View("~/Views/Test/Index.cshtml", item);
+        }
+    }
+}
---
+++ Models/TestClass.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApplication2.Models
+{
+    public class TestClass
+    {
+        public string itemdesc { get; set; }
+        public int UOM { get; set; }
+        public int quantity { get; set; }
+        public double UnitPrice { get; set; }
+        public string Discount { get; set; }
+        public double DiscAmt { get; set; }
+        public double Total { get; set; }
+        public int qtykecil { get; set; }
+
+    }
+}
